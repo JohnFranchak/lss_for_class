f1611bb33ca994a041737c4054b1bdb07085c8b7
John Franchak <franchak@gmail.com>
2020-02-27 11:08:02 -0800

    fixed graph example

--- lss1_summary_analyses.R
+++ lss1_summary_analyses.R
@@ -61,11 +61,11 @@ res_y <- lmer(std ~ task + (1|id),data = dsl_y)
 anova(res_y)
 
 # Graph the data =======
-ds_sum <-  dsl %>% group_by(dim, task) %>% filter(dim == "x") %>% 
+ds_sum <-  dsl %>% group_by(dim, task) %>% 
   summarise(stdev = mean(std, na.rm = T), n = n(), se = sd(std, na.rm = T)/sqrt(n), ymin = stdev - se, ymax = stdev + se) 
 
 #Plot summary
-ggplot(data = ds_sum, aes(x = task, group = task,ymin = ymin, ymax = ymax, y= stdev)) + 
+ggplot(data = filter(ds_sum, dim == "x"), aes(x = task, group = task,ymin = ymin, ymax = ymax, y= stdev)) + 
   geom_pointrange()
 
 #Plot individuals
@@ -80,8 +80,17 @@ ggplot() +
 #Make it pretty
 ggplot() + 
   geom_sina(data = filter(dsl, dim == "x"), aes(y = std, x = task, color = task), maxwidth = .5, position = position_dodge(.6), alpha = .5, size = 3) +
-  geom_errorbar(data = ds_sum, aes(x = task, group = task,ymin = ymin, ymax = ymax), size =1, width = .3, position = position_dodge(.6)) +
+  geom_errorbar(data = filter(ds_sum, dim == "x"), aes(x = task, group = task,ymin = ymin, ymax = ymax), size =1, width = .3, position = position_dodge(.6)) +
   scale_color_manual(values = cbp1[c(7,6)], guide = F) + 
-  scale_y_continuous(breaks = c(0,10,20,30,40), limits = c(0,40)) + 
+  scale_y_continuous(breaks = seq(0,20,5), limits = c(0,20)) + 
   labs(x = "Task", y = "Position SD (º)") 
 #ggsave("lss1_position_sd.pdf", units = "in", width = 4, height = 4)
+
+#Plot X and Y (need X and Color aes)
+ggplot() + 
+  geom_sina(data = dsl, aes(y = std, x = dim, color = task), maxwidth = .5, position = position_dodge(.6), alpha = .5, size = 3) +
+  geom_errorbar(data = ds_sum, aes(x = dim, group = task,ymin = ymin, ymax = ymax), size =1, width = .3, position = position_dodge(.6)) +
+  scale_color_manual(values = cbp1[c(7,6)], name = "Task") + 
+  scale_y_continuous(breaks = seq(0,20,5), limits = c(0,20)) + 
+  labs(x = "", y = "Position SD (º)") 
+
