09767d0e24b8a515df0b558abe2d48ba58833b61
John Franchak <franchak@gmail.com>
2020-02-27 11:00:30 -0800

    updated tutorial

--- lss1_summary_analyses.R
+++ lss1_summary_analyses.R
@@ -1,7 +1,10 @@
+# SET UP --------------
+#Install and load packages
 pkgs <- c("multcomp", "emmeans", "tidyverse", "effects", "lme4", "lmerTest", "psych","ggforce")
 lapply(pkgs[!(pkgs %in% installed.packages())], install.packages)
 lapply(pkgs, library, character.only = TRUE)
 
+#Set graphics themes and colors
 cbp1 <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
           "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 theme_update(text = element_text(size = 18),
@@ -17,21 +20,68 @@ ds <- read_csv("summary_stats_LSS1.csv", na = "NaN")
 ds <- mutate(ds, id = factor(id))
 ds[ds["id"] == 13, "search_eyex_speed"] <- NA
 
-# POSITION SD MEANS --------------
-dsl <-  gather(ds, key = "cond", value = "std", "walk_eyex_std","search_eyex_std","walk_eyey_std","search_eyey_std")
+# ANALYSES --------------
+
+# Convert from wide to long format =======
+dv_vars <- c("walk_eyex_std","search_eyex_std","walk_eyey_std","search_eyey_std")
+iv_vars <- c("id")
+
+ds %>% 
+  select(c(iv_vars,dv_vars)) %>% 
+  gather(key = "cond", value = "std", dv_vars) -> dsl
+
 dsl$dim <- factor(ifelse(is.na(str_extract(dsl$cond,"x")),"y","x"))
 dsl$task <- factor(ifelse(is.na(str_extract(dsl$cond,"walk")),"search","walk"),levels = c("walk","search"),labels = c("Walk","Search"))
 
-dsl %>% split(.$dim) %>% 
+# Let's summarize some data =======
+dsl %>% 
+  summarise(stdev = mean(std, na.rm = T), n = n(), se = sd(std, na.rm = T)/sqrt(n))
+
+dsl %>% 
+  filter(dim == "x") %>%
+  summarise(stdev = mean(std, na.rm = T), n = n(), se = sd(std, na.rm = T)/sqrt(n))
+
+dsl %>% 
+  group_by(dim, task) %>% 
+  summarise(stdev = mean(std, na.rm = T), n = n(), se = sd(std, na.rm = T)/sqrt(n))
+  
+# Let's split some data =======
+dsl %>% 
+  split(.$dim) %>% 
   map(~ lmer(std ~ task + (1|id),data = .)) %>% 
   map(anova)
 
-dsl %>% group_by(dim, task) %>% filter(dim == "x") %>% 
-  summarise(stdev = mean(std, na.rm = T), n = n(), se = sd(std, na.rm = T)/sqrt(n), ymin = stdev - se, ymax = stdev + se) %>% 
-  ggplot() + 
-  labs(x = "", y = "Position SD (º)") + 
+### Let's break what we did #####
+dsl_x <- filter(dsl, dim == "x")
+res_x <- lmer(std ~ task + (1|id),data = dsl_x)
+anova(res_x)
+
+dsl_y <- filter(dsl, dim == "y")
+res_y <- lmer(std ~ task + (1|id),data = dsl_y)
+anova(res_y)
+
+# Graph the data =======
+ds_sum <-  dsl %>% group_by(dim, task) %>% filter(dim == "x") %>% 
+  summarise(stdev = mean(std, na.rm = T), n = n(), se = sd(std, na.rm = T)/sqrt(n), ymin = stdev - se, ymax = stdev + se) 
+
+#Plot summary
+ggplot(data = ds_sum, aes(x = task, group = task,ymin = ymin, ymax = ymax, y= stdev)) + 
+  geom_pointrange()
+
+#Plot individuals
+ggplot(data = filter(dsl, dim == "x"), aes(y = std, x = task, color = task)) + 
+  geom_sina(maxwidth = .5, position = position_dodge(.6), alpha = .5, size = 3)
+
+#Plot both
+ggplot() + 
   geom_sina(data = filter(dsl, dim == "x"), aes(y = std, x = task, color = task), maxwidth = .5, position = position_dodge(.6), alpha = .5, size = 3) +
-  geom_errorbar(aes(x = task, group = task,ymin = ymin, ymax = ymax), size =1, width = .3, position = position_dodge(.6)) +
-  scale_color_manual(values = cbp1[c(7,6)], name = "Task") + 
-  scale_y_continuous(breaks = c(0,10,20,30,40), limits = c(0,40))
-ggsave("figures/lss1_position_sd.pdf", units = "in", width = 4, height = 4)
+  geom_errorbar(data = ds_sum, aes(x = task, group = task,ymin = ymin, ymax = ymax), size =1, width = .3, position = position_dodge(.6)) 
+
+#Make it pretty
+ggplot() + 
+  geom_sina(data = filter(dsl, dim == "x"), aes(y = std, x = task, color = task), maxwidth = .5, position = position_dodge(.6), alpha = .5, size = 3) +
+  geom_errorbar(data = ds_sum, aes(x = task, group = task,ymin = ymin, ymax = ymax), size =1, width = .3, position = position_dodge(.6)) +
+  scale_color_manual(values = cbp1[c(7,6)], guide = F) + 
+  scale_y_continuous(breaks = c(0,10,20,30,40), limits = c(0,40)) + 
+  labs(x = "Task", y = "Position SD (º)") 
+#ggsave("lss1_position_sd.pdf", units = "in", width = 4, height = 4)
